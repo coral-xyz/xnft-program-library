974b26d53257722e138beb606e55af196e5f1f17
wentokay <wentokay@users.noreply.github.com>
2022-08-25 15:01:09 -0400

    add basic iframe example

---
+++ packages/iframe/src/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactXnft, { AnchorDom, Iframe } from "react-xnft";
+
+ReactXnft.render(
+  <AnchorDom>
+    <Iframe src="https://example.com" />
+  </AnchorDom>
+);
