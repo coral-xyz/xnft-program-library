2a9bcea5c8abc9e5bd696d1b85d18d7d730b9065
Philipp Adrian <hello@philippadrian.com>
2022-10-12 17:11:35 -0300

    Tokenlist styling

--- packages/prices/src/App/TokenList.tsx
+++ packages/prices/src/App/TokenList.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { Loading, Text, View, TextField, useNavigation, ScrollBar } from "react-xnft";
+import { Loading, Text, View, Image, TextField, useNavigation, ScrollBar } from "react-xnft";
 import 'react-virtualized/styles.css'; // only needs to be imported once
 import { connect, StateType, useDispatch } from "../state";
 import { createSelector } from 'reselect';
 import { SET_FILTER } from "./_actions/SET_FILTER";
 import useRefreshTokenList from "./_hooks/useRefreshTokenList";
 import CenteredLoader from "./CenteredLoader";
+import { green, red } from "./_helpers/color";
 
 
 type Props = {
@@ -27,20 +28,26 @@ function TokenList({ filter, tokenInfo: tokenList }: Props & StateProps) {
     )
   }
   const regex = new RegExp(filter, "i");
-  const filteredList = tokenList.data.filter((token) => regex.test(token.name)||regex.test(token.symbol)||regex.test(token.id));
-  filteredList.length = 50;
+  const filteredList = tokenList.data.filter((token) => regex.test(token.name) || regex.test(token.symbol) || regex.test(token.id));
+  filteredList.length = 20;
   return (
     <View
       style={{
         display: "flex",
         flexDirection: "column",
-        height:"100%"
+        height: "100%",
+        padding:"10px 0px",
+        cursor:"pointer",
+        ":hover":{
+          backgroundColor:"red"
+        }
       }}
     >
       <View
         style={{
-          display:"flex",
-          padding: "0px 10px"
+          display: "flex",
+          padding:"0px 16px",
+          paddingBottom: "10px"
         }}
       >
         <TextField
@@ -50,29 +57,83 @@ function TokenList({ filter, tokenInfo: tokenList }: Props & StateProps) {
         />
       </View>
       <View
-      style={{
-        display:"flex",
-        flexGrow:1,
-        position:"relative"
-      }}
+        style={{
+          display: "flex",
+          flexGrow: 1,
+          position: "relative"
+        }}
       >
-      <ScrollBar>
-        {filteredList.map((token) => {
-          return (
-            <View
-              style={{
-                padding: "10px"
-              }}
-              key={token.id}
-              onClick={() => nav.push("details", token)}
-            >
-              <Text>{`${token.name} (${token.symbol}) ${token.id}`}</Text>
-            </View>
-          )
-        })}
-      </ScrollBar>
+        <ScrollBar>
+          {filteredList.map((token) => {
+            return (
+              <View
+                style={{
+                  padding: "8px 16px",
+                  display: "flex"
+                }}
+                key={token.id}
+                onClick={() => nav.push("details", token)}
+              >
+                <View
+                  style={{
+                    padding:"5px",
+                    paddingRight:"10px",
+                    display:"flex",
+                    alignItems:"center",
+                    justifyContent:"center"
+                  }}
+                  >
+                  <Image 
+                    style={{
+                      width: "34px",
+                      // padding:"5px"
+                    }}
+                    src={token.image}
+                  />
+                </View>
+                <View
+                  style={{
+                    display: "flex",
+                    flexGrow: 1,
+                    flexDirection: "column"
+                  }}
+                >
+                  <Text
+                    style={{
+                      lineHeight:"24px"
+                    }}
+                  >{`${token.name}`}</Text>
+                  <Text
+                    style={{
+                      opacity:0.5
+                    }}
+                  >{`${token.symbol.toLocaleUpperCase()}`}</Text>
+                </View>
+                <View
+                  style={{
+                    // width: "100px"
+                  }}
+                >
+                  <Text
+                    style={{
+                      textAlign:"right"
+                    }}
+                  >{`$${token.current_price}`}</Text>
+                  <Text
+                    style={{
+                      font: "Inter",
+                      fontSize: "16px",
+                      textAlign:"right",
+                      color: (token.price_change_percentage_24h??0)+0>0 ? green:red
+                    }}
+                  >{`${(token.price_change_percentage_24h??0)+0>0?"↗":"↘"} ${token.price_change_percentage_24h}%`}</Text>
+                </View>
+              </View>
+            )
+          })}
+        </ScrollBar>
       </View>
-      
+
     </View>
   )
 }
